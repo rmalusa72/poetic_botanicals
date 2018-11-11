750e21c443bd729bc954b6cc946c83ef4318ae78
Ruby M <ranmi.chaos.fox@gmail.com>
2018-11-11 16:00:58 -0500

    Add starter code to replace words with synonyms in an attempt to bring into meter

---
+++ synsound.py
@@ -0,0 +1,91 @@
+# synsound.py
+# Test of synonym replacement with meter and sound
+
+import re
+import thesaurus
+import pronouncing 
+
+STRESSED = True
+UNSTRESSED = False
+
+# Estimate the number of syllables in a word (poorly)
+def dirtysyllables(word):
+    vowels = "aeiouy"
+    invowelblock = False
+    syllables = 0
+    for letter in word:
+        if letter in vowels:
+            if not invowelblock:
+                invowelblock = True
+                syllables = syllables + 1
+        else:
+            invowelblock = False
+
+    return syllables 
+
+# Use pronouncing to get stresses for a word
+# Or return string of as many 3s as there are syllables if word is not in pronouncing dict
+def get_stresses(phrase):
+    words = phrase.split(" ")
+    if len(words) == 1:
+        return get_stresses_oneword(phrase)
+    else:
+        stresses = ""
+        for word in words:
+            stresses = stresses + get_stresses_oneword(word)
+        return stresses
+
+def get_stresses_oneword(word):
+    phones_list = pronouncing.phones_for_word(word)
+    if len(phones_list) > 0:
+        phones = phones_list[0]
+        stresses = pronouncing.stresses(phones)
+    else:
+        num_syllables = dirtysyllables(word)
+        stresses = '3' * num_syllables
+    return stresses
+
+# Attempt synonym replacement to construct metered (currently iambic) text
+def poetify(sentence):
+    
+    poem = []
+    next_syllable = UNSTRESSED
+
+    for word in sentence.split(" "):
+        print(word)
+
+        stresses = get_stresses(word)
+        if len(stresses) == 1:
+            stresses = "3" # Ignore the stress of one-syllable words
+        print(stresses)
+
+        if next_syllable == STRESSED:
+            desired_syl_pattern = r"[123]([03][123])*[03]*"
+        else:
+            desired_syl_pattern = r"[03]([123][03])*[123]*"
+        print(desired_syl_pattern)
+        print(re.match(desired_syl_pattern, stresses))
+        
+        if re.match(desired_syl_pattern, stresses):
+            poem.append(word)
+            if len(stresses) % 2 != 0:
+                next_syllable = not next_syllable
+        else:
+            w = thesaurus.Word(word)
+            synonyms = w.synonyms()
+            if len(synonyms) > 0:
+                good_synonyms = []
+                for synonym in synonyms:
+                    syn_stresses = get_stresses(synonym)
+                    if re.match(desired_syl_pattern, syn_stresses):
+                        good_synonyms.append(synonym)
+                if len(good_synonyms) > 0:
+                    poem.append(good_synonyms[0])
+                else:
+                    poem.append(word)
+            else:
+                poem.append(word)
+
+    return poem
+
+
