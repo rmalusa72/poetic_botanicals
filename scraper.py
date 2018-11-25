14bd826b2c2e4325e9da4fc1b57818da05696574
Ruby M <ranmi.chaos.fox@gmail.com>
2018-11-24 19:37:07 -0500

    Update scraper Update scraper to account for disambiguation pages whose disambiguationError's options' first entries point .. back to the same disambiguation page, by trying the next one.

--- scraper.py
+++ scraper.py
@@ -12,11 +12,35 @@ class Scraper:
 
     def scrape(self, query):
         pagename = wikipedia.search(query)[0]
-        try:
-            summary = wikipedia.summary(pagename)
-        except wikipedia.exceptions.DisambiguationError as e:
-            pagename = e.options[0]
-            summary = wikipedia.summary(pagename)
+        
+        success = False
+        last_pagename = pagename
+        index = 0
+        while not success:
+            try:
+                summary = wikipedia.summary(pagename)
+                success = True
+            except wikipedia.exceptions.DisambiguationError as e:
+                print("Excepting DisambiguationError for " + pagename)
+                print("options: " + str(e.options))
+                last_pagename = pagename
+                pagename = e.options[0]
+                if pagename == last_pagename:
+                    # Stuck in disambiguation loop
+                    index = index + 1
+                    if len(e.options) > index:
+                        pagename = e.options[index]
+
+
+        # try:
+        #     summary = wikipedia.summary(pagename)
+        # except wikipedia.exceptions.DisambiguationError as e:
+        #     print("Excepting DisambiguationError")
+        #     print(e.options)
+        #     pagename = e.options[0]
+        #     summary = wikipedia.summary(pagename)
+
+
         page = wikipedia.page(title=pagename)
         self.bib.write(pagename + "\n")
         return page.content
